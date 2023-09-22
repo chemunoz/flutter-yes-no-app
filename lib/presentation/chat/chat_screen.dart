6a90e9ed2cc7ca50273ed9a866ecd72510a33e28
Chema Muñoz <chemunoz78@gmail.com>
2023-09-22 09:50:58 +0200

    feat: create a new Stateless Widget for body (ChatView)

--- lib/presentation/chat/chat_screen.dart
+++ lib/presentation/chat/chat_screen.dart
@@ -6,18 +6,41 @@ class ChatScreen extends StatelessWidget {
   @override
   Widget build(BuildContext context) {
     return Scaffold(
-      appBar: AppBar(
-        leading: const Padding(
-          padding: EdgeInsets.all(4.0),
-          child: CircleAvatar(
-            backgroundImage: NetworkImage(
-                'https://info.mercadona.es/img-cont/es/1992-img.jpg'),
+        appBar: AppBar(
+          leading: const Padding(
+            padding: EdgeInsets.all(4.0),
+            child: CircleAvatar(
+              backgroundImage: NetworkImage(
+                  'https://info.mercadona.es/img-cont/es/1992-img.jpg'),
+            ),
           ),
+          title: const Row(
+            children: [Text('Decide App'), Icon(Icons.ac_unit)],
+          ),
+          centerTitle: false,
         ),
-        title: Row(
-          children: [const Text('Si-No App'), const Icon(Icons.ac_unit)],
+        body: _ChatView());
+  }
+}
+
+class _ChatView extends StatelessWidget {
+  @override
+  Widget build(BuildContext context) {
+    return SafeArea(
+      child: Padding(
+        padding: const EdgeInsets.symmetric(horizontal: 10),
+        child: Column(
+          children: [
+            Expanded(
+                child: ListView.builder(
+              itemCount: 100,
+              itemBuilder: (context, index) {
+                return Text('Indice: $index');
+              },
+            )),
+            const Text('Mundo')
+          ],
         ),
-        centerTitle: false,
       ),
     );
   }
